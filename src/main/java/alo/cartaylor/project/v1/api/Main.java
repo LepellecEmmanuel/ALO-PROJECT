67498f593c04e72f3aa1be4964db4d72df859d1c
EmmanuelLepellec <emmanuel.lepellec@orange.fr>
2025-11-13 10:33:01 +0100

    improve terminal interface

--- src/main/java/alo/cartaylor/project/v1/api/Main.java
+++ src/main/java/alo/cartaylor/project/v1/api/Main.java
@@ -6,8 +6,38 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
+
+    static String getConfiguration(Configurator configurator) {
+        String result = "";
+        Set<PartType> selection = configurator.getConfiguration().getSelectedParts();
+        if(selection.isEmpty()) {
+            result = "No parts selected.";
+            return result;
+        } else {
+            result = "Selection(";
+            for(PartType partType : selection) {
+                result += partType.getCategory().getName() +":"+partType.getName()+":"+checkPartType(partType, configurator)+",  ";
+            }
+            return result += ")";
+        }
+
+    }
+    static String checkPartType(PartType partType, Configurator configurator) {
+        String result = "Requirements(";
+        Set<PartType> requirements = configurator.getCompatibilityChecker().getRequirements(partType);
+        for(PartType requirement: requirements) {
+            result += requirement.getName()+",";
+        }
+        result += ") Incompatibilities(";
+        Set<PartType> incompats = configurator.getCompatibilityChecker().getIncompatibilities(partType);
+        for(PartType incompat: incompats) {
+            result += incompat.getName()+",";
+        }
+        return result += ")";
+    }
     static void addPartType(Configurator configurator, Scanner scanner) {
         Category[] category = configurator.getCategories().toArray(Category[]::new);
+        System.out.println(getConfiguration(configurator));
         System.out.println("Categories:");
         int choice;
         for(int i = 0; i < category.length; i++) {
@@ -19,9 +49,10 @@ public class Main {
             choice = scanner.nextInt();
         }
         System.out.println(category[choice].getName() + " selected.");
+        System.out.println(getConfiguration(configurator));
         PartType[] partTypes = configurator.getVariants(category[choice]).toArray(PartType[]::new);
         for(int j = 0; j < partTypes.length; j++) {
-            System.out.println(j + ". " + partTypes[j].getName());
+            System.out.println(j + ". " + partTypes[j].getName() + "  " + checkPartType(partTypes[j], configurator));
         }
         System.out.print("Select part type: ");
         choice = scanner.nextInt();
@@ -35,38 +66,47 @@ public class Main {
     public static void main(String[] args) {
         Configurator configurator = new ConfiguratorImpl();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello");
         while(true) {
            System.out.println("0. Change configuration");
            System.out.println("1. See configuration");
-           System.out.println("2. See if configuration status");
+           System.out.println("2. See configuration status");
            System.out.println("3. Reset configuration");
+           System.out.println("4. Validate configuration and Quit");
            System.out.print("choose option: ");
            int choice = scanner.nextInt();
-           if(choice < 0 || choice > 3) {
+           if(choice < 0 || choice > 4) {
+               System.out.print("choose another option: ");
                choice = scanner.nextInt();
            }
            if(choice == 0) {
+               System.out.println();
                addPartType(configurator, scanner);
+               System.out.println();
            }
            if(choice == 1) {
-               Set<PartType> selection = configurator.getConfiguration().getSelectedParts();
-               if(selection.isEmpty()) {
-                   System.out.println("No parts selected.");
-               }
-               for(PartType partType : selection) {
-                   System.out.println(partType.getName() + " selected.");
-               }
+               System.out.println();
+               System.out.println(getConfiguration(configurator));
+               System.out.println();
            }
            if(choice == 2) {
+               System.out.println();
                System.out.println("Configuration completed:" + configurator.getConfiguration().isCompleted());
                System.out.println("Configuration valid:" + configurator.getConfiguration().isValid());
+               System.out.println();
            }
            if(choice == 3) {
+               System.out.println();
                configurator.getConfiguration().clear();
                System.out.println("Configuration cleared.");
+               System.out.println();
+           }
+           if(choice == 4) {
+               if(!configurator.getConfiguration().isValid() || !configurator.getConfiguration().isCompleted()) {
+                   System.out.println("Cannot save configuration because it is not valid or complete.");
+               }
+               scanner.close();
+               return;
            }
         }
-        //scanner.close();
     }
 }
